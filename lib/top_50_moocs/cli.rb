323d9b149c27dae9da7783c31aa2ca43d64791e5
Seb Gertz <sebgertz@gmail.com>
2017-07-13 16:36:24 +0000

    updated cli

---
+++ lib/top_50_moocs/cli.rb
@@ -0,0 +1,8 @@
+class CLI
+
+  def call
+    puts "Welcome to Top 50 MOOCs (Massive Open Online Courses)!"
+    puts "Please enter 'all' for the full list. To see only one part, enter either '1-24' or '25-50'."
+  end
+
+end
