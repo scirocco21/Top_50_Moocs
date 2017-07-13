d82ba4f3f833095d1a659531006be5adf38f6ef8
Seb Gertz <sebgertz@gmail.com>
2017-07-13 20:38:57 +0000

    user interface

--- lib/top_50_moocs/cli.rb
+++ lib/top_50_moocs/cli.rb
@@ -1,8 +1,12 @@
 class CLI
 
   def call
-    puts "Welcome to Top 50 MOOCs (Massive Open Online Courses)!"
+    puts "Welcome to the Top 50 MOOCs (Massive Open Online Courses)!"
     puts "Please enter 'all' for the full list. To see only one part, enter either '1-24' or '25-50'."
+    course_list
+    puts "Enter the course number for more information or type 'exit' to quite the program."
+    details_list
+    puts "Enter 'return' to go back to the menu or type 'exit' to quit the program."
   end
 
 end
