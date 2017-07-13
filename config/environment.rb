789782bb8fcd7995e2938b832351e6186ac75daa
Seb Gertz <sebgertz@gmail.com>
2017-07-13 14:27:39 +0000

    File structure changed; config addded

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/top_50_moocs/scraper'
+require_relative '../lib/top_50_moocs/course'
+require_relative '../lib/top_50_moocs/cli'
--- lib/Top_50_Moocs.rb
+++
@@ -1,5 +0,0 @@
-require "Top_50_Moocs/version"
-
-module Top50Moocs
-  # Your code goes here...
-end
