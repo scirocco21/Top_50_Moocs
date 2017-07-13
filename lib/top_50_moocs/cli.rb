ed98e8bf3dbfca8e5846c942c19a35ee8138c300
Seb Gertz <sebgertz@gmail.com>
2017-07-13 22:15:31 +0000

    added create and display methdods for courses

--- lib/top_50_moocs/cli.rb
+++ lib/top_50_moocs/cli.rb
@@ -3,10 +3,17 @@ class CLI
   def call
     puts "Welcome to the Top 50 MOOCs (Massive Open Online Courses)!"
     puts "Please enter 'all' for the full list. To see only one part, enter either '1-24' or '25-50'."
-    course_list
+    make_course_list
+    show_course_list
     puts "Enter the course number for more information or type 'exit' to quite the program."
-    details_list
+    get_info
+    show_info
     puts "Enter 'return' to go back to the menu or type 'exit' to quit the program."
   end
+
+  def make_course_list
+    course_list = Scraper.scrape_courses
+  end
+
 
 end
