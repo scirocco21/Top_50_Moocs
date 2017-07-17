08a4884e0f26fe2836b3edf7bd940a3395ad1de9
Seb Gertz <sebgertz@gmail.com>
2017-07-17 12:35:24 +0000

    cli updated, prints urls correctly

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,9 +3,16 @@ class CLI
   def call
     puts "Welcome to popular_moocs, a list of all Massive Open Online Courses featured as 'popular' on Coursera!"
     puts "Press enter to see the full list."
-    make_courses
-    show_course_list
+    if gets == "\n"
+      make_courses
+      show_course_list
+    end
     puts "Enter 'return' to go back to the menu or type 'exit' to quit the program."
+    if gets.strip.downcase == "return"
+      call
+    elsif gets.strip.downcase == "exit"
+      exit
+    end
   end
 
   def make_courses
@@ -14,8 +21,13 @@ class CLI
   end
 
   def show_course_list
+    list_counter = 1
     Course.all.each do |course|
-      puts course.title
+      puts "#{list_counter}. #{course.title}"
+      puts "Course provider: #{course.institution}"
+      puts "Link: https://www.coursera.org" + "#{course.url}"
+      puts ""
+      list_counter += 1
     end
   end
 
