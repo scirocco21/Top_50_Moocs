0db9464d7c210e809922aedbed47850202413486
Seb Gertz <sebgertz@gmail.com>
2017-07-17 11:03:00 +0000

    course class can collect title, institution and url

--- lib/course.rb
+++ lib/course.rb
@@ -1,12 +1,29 @@
 class Course
-  attr_accessor :title, :description, :url
+  attr_accessor :title, :institution, :url
 
-  @@all = []
+  @@course_list = []
 
-  def initialize(courses_hash)
-    courses_hash.each do |attribute, value|
+  def initialize(course_hash)
+    course_hash.each do |attribute, value|
       self.send("#{attribute}=", value)
     end
-  @@all << self
+    @@course_list << self
+  end
+
+  def self.create_courses(courses)
+    courses.each do |course_hash|
+      Course.new(course_hash)
+    end
+  end
+
+  def show_course_list
+    Course.all.each do |course|
+      puts course.title
+    end
+  end
+
+  def self.all
+    @@course_list
+  end
 
 end
