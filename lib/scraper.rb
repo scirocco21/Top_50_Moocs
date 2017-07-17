09325019391426b4d7ddb7d678e9183354b6a76c
Seb Gertz <sebgertz@gmail.com>
2017-07-17 11:01:55 +0000

    top level scraper working

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,11 +6,12 @@ class Scraper
     doc = Nokogiri::HTML(html)
     course_cards = doc.css('a.rc-OfferingCard.nostyle')
     course_cards.each do |course|
-      course_title = course.css('h2.color-primary-text.headline-1-text.flex-1')
-      course_provider = course.css('div.text-light.offering-partner-names')
-      course_url = course.css('href')
-      courses << {name: course_title, institution: course_provider, url = course_url}
+      course_title = course.css('h2.color-primary-text.headline-1-text.flex-1').text
+      course_provider = course.css('div.text-light.offering-partner-names').text
+      course_url = course.css('href').text
+      courses << {title: course_title, institution: course_provider, url: course_url}
     end
+    courses
   end
-  courses
+  
 end
