ff5b5933d3e64b4d8d305acc14c3a7cccc982d3b
Seb Gertz <sebgertz@gmail.com>
2017-07-17 11:01:19 +0000

    basic cli functionality

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,22 +3,20 @@ class CLI
   def call
     puts "Welcome to popular_moocs, a list of all Massive Open Online Courses featured as 'popular' on Coursera!"
     puts "Press enter to see the full list."
-    make_course_list
-    show_course_list(course_list)
-    puts "Enter the course number for more information or type 'exit' to quite the program."
-    input = gets.strip.downcase
-    show_course_details(input) unless input == "exit"
+    make_courses
+    show_course_list
     puts "Enter 'return' to go back to the menu or type 'exit' to quit the program."
   end
 
-  def make_course_list
-    course_list = Scraper.scrape_courses
+  def make_courses
+    courses = Scraper.scrape_courses
+    Course.create_courses(courses)
   end
 
-  def show_course_list(course_list)
-    course_list.each {|course| puts course}
+  def show_course_list
+    Course.all.each do |course|
+      puts course.title
+    end
   end
-
-
 
 end
